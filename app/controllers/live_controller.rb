2a03254b2018c55ad3fa1a3810cd7656a3758925
dlatham <dave.latham@gmail.com>
2018-03-26 14:54:35 -0700

    added yaml to test without hitting CSE

--- app/controllers/live_controller.rb
+++ app/controllers/live_controller.rb
@@ -1,6 +1,7 @@
 class LiveController < ApplicationController
 	require 'net/http'
 	require 'google_custom_search_api'
+	require 'yaml'
 
 	def show
 		@test = Test.find(params[:id])
@@ -9,7 +10,10 @@ class LiveController < ApplicationController
 
 	def results
 		@test = Test.find(params[:id])
-		@serp = GoogleCustomSearchApi.search(params[:q])
+		#for testing purposes (don't hit the rate limiter on the CSE API)
+		file = File.join(Rails.root, 'config', 'test-serp-data.yml')
+		@serp = YAML.load_file(file)
+		#@serp = GoogleCustomSearchApi.search(params[:q])
 		#logger.debug(@serp)
 	end
 
